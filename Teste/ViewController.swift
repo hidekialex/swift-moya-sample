3eae784fafa957a00a6631c87fd00fe5e1d51ca0
hidekialex <hideki.alex@hotmail.com>
2019-06-06 02:51:52 -0300

    Update ViewController.swift

--- Teste/ViewController.swift
+++ Teste/ViewController.swift
@@ -29,10 +29,18 @@ class ViewController: UIViewController {
          */
         let card = Card(nome: "Goblin", cor: "Red", ataque: 2, defesa: 3)
         
-        //
+        // Passar o o enum criarCarta declarado em Servicos
         chamador.request(Servicos.criarCarta(card: card)) { (result) in
             switch result {
                 case .success(let response):
+                    //Receita de bolo para converter o retorno do servidor em um objeto
+                    //Passar no decode a classe que representa o JSON (no caso Response.self)
+                    /**
+                        {
+                            "statusCode":200,
+                            "body": "mensagem retorno"
+                        }   
+                    **/
                     let resposta = try! JSONDecoder().decode(Response.self, from: response.data)
                     print(resposta)
                 case .failure(let error):
